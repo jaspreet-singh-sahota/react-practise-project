a493ebaa8dc3e36136cae8c6dcf7db6bd5cdc564
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-21 19:34:10 +0530

    Change Greet function component to arrow functional component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-function Greet() {
+export const Greet = () => {
   return (
     <div>
-      <h1>
-      Hello Jassi
-      </h1>
+      <h1>Hello Jassi</h1>
     </div>
   )
 }
+
 
 export default Greet
