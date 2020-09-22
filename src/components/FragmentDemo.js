a5139544033f5a65c8601a22f3237a0165a5b423
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-22 22:42:42 +0530

    Wrap elements with React.Fragment instead of a div

--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const FragmentDemo = () => {
   return (
-    <div>
-      Fragment Demo
-    </div>
+    <Fragment>
+      <h1>
+        Fragment Demo
+      </h1>
+      <p>
+        This is the fragment component
+      </p>
+    </Fragment>
   )
 }
 
