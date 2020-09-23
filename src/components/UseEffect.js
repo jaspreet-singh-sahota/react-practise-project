45b7871877d4904f9c2b0a74c032e928114b79f4
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-23 21:00:17 +0530

    Add useEffect comparision od previous and current state

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from 'react'
 const UseEffect1 = () => {
 
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
   useEffect(() => {
+    console.log('hello')
     document.title = `You have Clicked ${count} times`
-  })
+  }, [count])
 
 
   return (
     <div>
+      <input type="text" name={name} onChange={(e) => setName({name: e.target.value})}/>
       <button onClick={() => setCount(previousCount => previousCount + 1)}>Click here</button>
     </div>
   )
