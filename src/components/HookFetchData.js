a14b4f7a23cd881ea79c2cb33fff2c9d19c3707a
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-24 00:35:52 +0530

    Fetch a post with hook

--- src/components/HookFetchData.js
+++ src/components/HookFetchData.js
@@ -2,23 +2,36 @@ import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 const HookFetchData = () => {
-  const [posts, setPost] = useState([])
+  const [post, setPost] = useState({})
+  const [id , setId] = useState(1)
+  const [idFromButton, setIdFromButton] = useState(id)
+
+  const idHandler = (e) => {
+    setId(e.target.value)
+  }
+
+  const buttonHandler = () => {
+    setIdFromButton(id)
+  }
 
   useEffect(() => {
-    Axios.get('https://jsonplaceholder.typicode.com/posts')
+    Axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
       .then(response => {
-      console.log(response)
-      setPost(response.data)
+        console.log(response)
+        setPost(response.data)
       }).catch(error => {
         console.log(error)
       })
-  }, [])
+  }, [idFromButton])
 
   return (
     <div>
-      <ul>
+      <input type="text" value={id} onChange={idHandler}/>
+      <button onClick={buttonHandler}>Click here</button>
+      <div>{post.title}</div>
+      {/* <ul>
         {posts.map(post => <li key={post.id}>{post.title}</li>)}
-      </ul>
+      </ul> */}
     </div>
   )
 }
